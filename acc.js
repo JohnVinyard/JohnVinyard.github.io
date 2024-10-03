ac23e4262e4354e6d96fb15183a83e8a18d8a0e1
John Vinyard <johnwho@gmail.com>
2024-10-02 19:02:19 -0500

    Remove code referencing deleted HTML elements

--- acc.js
+++ acc.js
@@ -31,11 +31,6 @@ const unitInterval = new Interval(0, 1);
 
 document.addEventListener("DOMContentLoaded", async (event) => {
   const start = document.getElementById("start-demo");
-  // const tester = document.getElementById("test");
-  // const recorder = document.getElementById("recorder");
-  // const filterReadout = document.getElementById("filter");
-  // filterReadout.style.backgroundColor = "#eee";
-  // filterReadout.innerText = 500;
 
   const context = new AudioContext();
 
@@ -65,63 +60,6 @@ document.addEventListener("DOMContentLoaded", async (event) => {
     return audioBufferPromise;
   };
 
-  // karplus.addEventListener("click", async () => {
-  //   const osc = context.createOscillator();
-
-  //   const scriptNode = context.createScriptProcessor(512);
-
-  //   const mainGain = context.createGain();
-  //   const delayGain = context.createGain();
-  //   delayGain.gain.setValueAtTime(0.75, context.currentTime);
-
-  //   const delay = context.createDelay(1);
-  //   delay.delayTime.setValueAtTime(0.015, context.currentTime);
-
-  //   const filter = context.createBiquadFilter();
-  //   filter.type = "lowpass";
-  //   filter.frequency.setValueAtTime(800, context.currentTime);
-
-  //   osc.connect(scriptNode);
-  //   scriptNode.connect(mainGain);
-  //   mainGain.connect(context.destination);
-
-  //   mainGain.connect(delay);
-  //   delay.connect(delayGain);
-  //   delayGain.connect(filter);
-  //   filter.connect(delay);
-
-  //   filter.connect(context.destination);
-
-  //   mainGain.gain.exponentialRampToValueAtTime(1, context.currentTime + 0.001);
-  //   mainGain.gain.exponentialRampToValueAtTime(
-  //     0.000001,
-  //     context.currentTime + 0.2
-  //   );
-
-  //   scriptNode.addEventListener(
-  //     "audioprocess",
-  //     ({ inputBuffer, outputBuffer }) => {
-  //       const output = outputBuffer.getChannelData(0);
-
-  //       for (let i = 0; i < output.length; i++) {
-  //         output[i] = Math.random() * 2 - 1;
-  //       }
-  //     }
-  //   );
-
-  //   osc.start();
-
-  //   setTimeout(() => {
-  //     osc.disconnect(mainGain);
-  //     mainGain.disconnect(scriptNode);
-  //     scriptNode.disconnect(context.destination);
-  //     scriptNode.disconnect(delay);
-  //     delay.disconnect(context.destination);
-  //     delay.disconnect(filter);
-  //     filter.disconnect(scriptNode);
-  //   }, 30000);
-  // });
-
   class ConvUnit {
     initialized = false;
     gain = null;
@@ -143,17 +81,7 @@ document.addEventListener("DOMContentLoaded", async (event) => {
       filter.type = "lowpass";
       filter.frequency.setValueAtTime(500, context.currentTime);
 
-      conv.buffer = await fetchAudio(
-        this.url,
-        // "https://nsynth.s3.amazonaws.com/bass_electronic_018-036-100",
-        context
-      );
-
-      // gainNode.gain.exponentialRampToValueAtTime(1, context.currentTime + 0.001);
-      // gainNode.gain.exponentialRampToValueAtTime(
-      //   0.000001,
-      //   context.currentTime + 0.2
-      // );
+      conv.buffer = await fetchAudio(this.url, context);
 
       scriptNode.addEventListener(
         "audioprocess",
@@ -225,8 +153,6 @@ document.addEventListener("DOMContentLoaded", async (event) => {
 
   const activeNotes = new Set(["C"]);
 
-  // const unit = new ConvUnit();
-
   const notes = {
     C: "https://nsynth.s3.amazonaws.com/bass_electronic_018-036-100",
     E: "https://nsynth.s3.amazonaws.com/bass_electronic_018-040-127",
@@ -255,30 +181,17 @@ document.addEventListener("DOMContentLoaded", async (event) => {
     document.addEventListener(
       "mousemove",
       ({ movementX, movementY, clientX, clientY }) => {
-        // console.log(event);
-
-        // const x = document.getElementById("mousex");
-        // x.innerText = movementX;
-
-        // const y = document.getElementById("mousey");
-        // y.innerText = movementY;
-
         if (Math.abs(movementX) > 10 || Math.abs(movementY) > 10) {
           // unit.trigger(1);
           unit.trigger(
             Array.from(activeNotes).map((an) => notes[an]),
             1
           );
-
-          // if (debug) {
-          //   recorder.innerText += ".";
-          // }
         }
 
         const u = vertical.translateTo(clientY, unitInterval);
         const hz = unitInterval.translateTo(u ** 2, filterCutoff);
         unit.updateCutoff(hz);
-        // filterReadout.innerText = hz.toFixed(1);
       }
     );
   };
@@ -289,31 +202,15 @@ document.addEventListener("DOMContentLoaded", async (event) => {
         DeviceMotionEvent.requestPermission();
       }
 
-      // TODO: orientation should determine the mix between three different room sounds
-      const orientationFields = ["alpha", "gamma", "beta"];
-
       window.addEventListener("deviceorientationabsolute", (event) => {
-        orientationFields.forEach((field) => {
-          const el = document.getElementById(field);
-          el.innerText = event[field];
-        });
-
         const u = gamma.translateTo(event.gamma, unitInterval);
         const hz = unitInterval.translateTo(u ** 4, filterCutoff);
-        filterReadout.innerText = hz.toFixed(1);
         unit.updateCutoff(hz);
       });
-
-      const motionFieldIds = ["x", "y", "z"];
 
       window.addEventListener(
         "devicemotion",
         (event) => {
-          motionFieldIds.forEach((field) => {
-            const el = document.getElementById(field);
-            el.innerText = event.acceleration[field];
-          });
-
           const threshold = 4;
 
           // TODO: maybe this trigger condition should be the norm as well?
@@ -328,17 +225,10 @@ document.addEventListener("DOMContentLoaded", async (event) => {
                 event.acceleration.z ** 2
             );
 
-            if (debug) {
-              recorder.innerText += ".";
-            }
-
             unit.trigger(
               Array.from(activeNotes).map((an) => notes[an]),
               norm * 0.2
             );
-
-            // unit.trigger(norm * 0.2);
-            // playRoomSound();
           }
         },
         true
@@ -349,12 +239,9 @@ document.addEventListener("DOMContentLoaded", async (event) => {
     }
   };
 
-  start.addEventListener("click", async () => {
+  start.addEventListener("click", async (event) => {
     useAcc();
     useMouse();
+    event.target.disabled = true;
   });
-
-  // tester.addEventListener("click", async () => {
-  //   unit.trigger();
-  // });
 });
