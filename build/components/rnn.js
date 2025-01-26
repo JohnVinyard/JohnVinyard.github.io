8d149764eb83506846f2f03229edbf0fcb0af93d
John Vinyard <johnwho@gmail.com>
2025-01-26 14:49:28 -0600

    Maybe-working full RNN implementation

--- build/components/rnn.js
+++ build/components/rnn.js
@@ -1,14 +1,42 @@
+const twoDimArray = (data, shape) => {
+    const [x, y] = shape;
+    const output = [];
+    for (let i = 0; i < data.length; i += y) {
+        output.push(data.slice(i, i + y));
+    }
+    return output;
+};
+const vectorVectorDot = (a, b) => {
+    return a.reduce((accum, current, index) => {
+        return accum + current * b[index];
+    }, 0);
+};
+const elementwiseSum = (a, b) => {
+    return a.map((value, index) => value + b[index]);
+};
+/**
+ * e.g., if vetor is length 64, and matrix is (128, 64), we'll end up
+ * with a new vector of length 128
+ */
+const dotProduct = (vector, matrix) => {
+    return new Float32Array(matrix.map((v) => vectorVectorDot(v, vector)));
+};
 class Rnn extends AudioWorkletProcessor {
     constructor(options) {
         super();
+        this.eventQueue = [];
         console.log('Constructing RNN Worklet');
         console.log(options);
-        // console.log(inProjection);
-        // console.log(outProjection);
-        // console.log(rnnInProjection);
-        // console.log(rnnOutProjection);
+        const ctorArgs = options.processorOptions;
+        this.controlPlaneDim = ctorArgs.inProjection.shape[0];
+        const hiddenDim = ctorArgs.rnnInProjection.shape[1];
+        this.rnnHiddenState = new Float32Array(hiddenDim).fill(0);
+        this.inProjection = twoDimArray(ctorArgs.inProjection.array, ctorArgs.inProjection.shape);
+        this.rnnInProjection = twoDimArray(ctorArgs.rnnInProjection.array, ctorArgs.rnnInProjection.shape);
+        this.rnnOutProjection = twoDimArray(ctorArgs.rnnOutProjection.array, ctorArgs.rnnOutProjection.shape);
+        this.outProjection = twoDimArray(ctorArgs.outProjection.array, ctorArgs.outProjection.shape);
         this.port.onmessage = (event) => {
-            console.log('PONG', event.data);
+            this.eventQueue.push(event.data);
         };
     }
     /**
@@ -16,7 +44,25 @@ class Rnn extends AudioWorkletProcessor {
      */
     process(inputs, outputs, parameters) {
         for (let i = 0; i < outputs.length; i++) {
+            // outputs
             for (let j = 0; j < outputs[i].length; j++) {
+                const maybeControlPlane = this.eventQueue.shift();
+                // https://pytorch.org/docs/stable/generated/torch.nn.RNN.html
+                if (maybeControlPlane) {
+                    const controlPlane = maybeControlPlane !== null && maybeControlPlane !== void 0 ? maybeControlPlane : new Float32Array(this.controlPlaneDim).fill(0);
+                    const inp = dotProduct(controlPlane, this.inProjection);
+                    console.log('INPUT', inp.length);
+                    const rnnInp = dotProduct(inp, this.rnnInProjection);
+                    console.log('RNN IN', rnnInp.length);
+                    const rnnHidden = dotProduct(this.rnnHiddenState, this.rnnOutProjection);
+                    console.log('RNN HIDDEN', rnnHidden.length);
+                    const summed = elementwiseSum(rnnInp, rnnHidden);
+                    const nonlinearity = summed.map(Math.tanh);
+                    console.log('NONLINEARITY', nonlinearity.length);
+                    const output = dotProduct(nonlinearity, this.outProjection);
+                    console.log('OUPUT', output.length);
+                }
+                // channels
                 outputs[i][j].set([Math.random() * 2 - 1]);
             }
         }
